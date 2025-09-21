63526013dae37f1503087112b38fe0cc3301245b
shivam kumar <shivamk2000.com@gmail.com>
2025-09-21 11:18:37 +0530

    fixing vercell error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import ErrorBoundary from "./lib/ErrorBoundary.tsx";
 import App from "./App.tsx";
 import "./index.css";
@@ -12,10 +11,8 @@ if (!clientId) {
 }
 root.render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
       <ErrorBoundary >
         <App />
       </ErrorBoundary>
-    </GoogleOAuthProvider>
   </StrictMode>
 );
